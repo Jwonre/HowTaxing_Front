e8cf43455ce147dac98f79412c33b174d29326aa
yoojaeok <squall3309@gmail.com>
2024-12-31 08:51:58 +0900

    [기타]   ios 셋팅

--- ios/HowTaxingRelease/AppDelegate.mm
+++ ios/HowTaxingRelease/AppDelegate.mm
@@ -1,7 +1,10 @@
 #import "AppDelegate.h"
 
+#import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
-#import <RNKakaoLogins.h>
+#import <React/RCTRootView.h>
+#import "RNKakaoLogins.h"
+#import <NaverThirdPartyLogin/NaverThirdPartyLoginConnection.h>
 
 @implementation AppDelegate
 
@@ -11,14 +14,18 @@
      options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
   
   // naver
-    if ([url.scheme isEqualToString:@"{{ CUSTOM URL SCHEME }}"]) {
-      return [[NaverThirdPartyLoginConnection getSharedInstance] application:app openURL:url options:options];
+    if ([url.scheme isEqualToString:@"{{howtaxingrelease}}"]) {
+      return [[NaverThirdPartyLoginConnection getSharedInstance] application:application openURL:url options:options];
     }
   
   //kakao
- if([RNKakaoLogins isKakaoTalkLoginUrl:url]) {
-    return [RNKakaoLogins handleOpenUrl: url];
- }
+  dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(void){
+        dispatch_async(dispatch_get_main_queue(), ^(void){
+          if ([RNKakaoLogins isKakaoTalkLoginUrl:url]) {
+            [RNKakaoLogins handleOpenUrl: url];
+          }
+        });
+    });
  return NO;
 }
 
---
+++ ios/Untitled.h
@@ -0,0 +1,7 @@
+//
+//  Untitled.h
+//  HowTaxingRelease
+//
+//  Created by 유재옥 on 12/30/24.
+//
+
