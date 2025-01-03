79d079a62b6f8558218dd5987d0791273dad0ad6
yoojaeok <squall3309@gmail.com>
2025-01-03 21:13:34 +0900

    [기타]   ios 설정

--- ios/HowTaxingRelease/AppDelegate.mm
+++ ios/HowTaxingRelease/AppDelegate.mm
@@ -1,7 +1,10 @@
 #import "AppDelegate.h"
 
+#import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
-#import <RNKakaoLogins.h>
+#import <React/RCTRootView.h>
+#import "RNKakaoLogins.h"
+#import <NaverThirdPartyLogin/NaverThirdPartyLoginConnection.h>
 
 @implementation AppDelegate
 
@@ -12,7 +15,7 @@
   
   // naver
     if ([url.scheme isEqualToString:@"{{ CUSTOM URL SCHEME }}"]) {
-      return [[NaverThirdPartyLoginConnection getSharedInstance] application:app openURL:url options:options];
+      return [[NaverThirdPartyLoginConnection getSharedInstance] application:application openURL:url options:options];
     }
   
   //kakao
