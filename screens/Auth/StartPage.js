124e339b43416d8bedc4d377890ee1736772718d
yoojaeok <squall3309@gmail.com>
2025-01-15 09:20:08 +0900

    [기타]  인트로 수정

--- screens/Auth/StartPage.js
+++ screens/Auth/StartPage.js
@@ -1,29 +1,17 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, View, Platform } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View } from 'react-native';
 import LottieView from 'lottie-react-native';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { setStartPage } from '../../redux/startPageSlice.js';
 
 export default function SplashScreen(props) {
   const dispatch = useDispatch();
   const { navigation } = props;
-
   useEffect(() => {
     navigation.setOptions({
       headerShown: false,
     });
   }, [navigation]);
-
-  useEffect(() => {
-    if (Platform.OS === 'ios') {
-      // iOS에서 타이머를 사용해 상태 업데이트
-      const timer = setTimeout(() => {
-        console.log('Animation Finished via Timer (iOS)');
-        dispatch(setStartPage(false));
-      }, 3000); // 애니메이션 길이에 맞춰 설정
-      return () => clearTimeout(timer); // 컴포넌트 언마운트 시 타이머 클리어
-    }
-  }, [dispatch]);
 
   return (
     <View style={styles.container}>
@@ -31,14 +19,9 @@ export default function SplashScreen(props) {
         source={require('../../assets/animation/json/intro_logo.json')}
         autoPlay
         loop={false}
-        onAnimationFinish={
-          Platform.OS === 'android'
-            ? () => {
-                console.log('Animation Finished via onAnimationFinish (Android)');
-                dispatch(setStartPage(false));
-              }
-            : undefined // iOS는 onAnimationFinish 사용 안 함
-        }
+        onAnimationFinish={() => {
+          dispatch(setStartPage(false));
+        }}
         style={{ width: '100%', height: '100%' }}
       />
     </View>
